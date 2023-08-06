c2f75b6a12d388dae87482d33cecb17957f9bc9c
Mykola Travnik <mykola.travnik@outlook.com>
2023-08-06 20:52:57 +0300

    fix

--- hospital-client/README.md
+++ hospital-client/README.md
@@ -1,6 +1,6 @@
 # HospitalClient
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.8.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.7.
 
 ## Development server
 
--- hospital-client/src/app/app.component.ts
+++ hospital-client/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
-  imports: [MatButtonModule, MatDividerModule, MatIconModule]
+  imports: [CommonModule, RouterOutlet, MatButtonModule, MatDividerModule, MatIconModule],
 })
 export class AppComponent {
 }
