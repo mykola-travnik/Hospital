b6f7f7288387dfe424461e1bb9deeb85e4e965f3
Mykola Travnik <mykola.travnik@gmail.com>
2023-08-13 00:07:34 +0300

    fix

---
+++ dotnet/App/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        [HttpGet(nameof(LogIn))]
+        public async Task<string> LogIn(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost(nameof(SignIn))]
+        public async Task<UserDto> SignIn(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dotnet/Business/Services/AuthService.cs
@@ -0,0 +1,23 @@
+﻿namespace Business.Services
+{
+    public class AuthService
+    {
+        private readonly IUserService userService;
+
+        public AuthService(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<string> LogIn(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<UserDto> SignIn(string username, string password)
+        {
+            throw new NotImplementedException();
+            //var newUser = new UserCreateDto() { Name = username, Roles = new()}
+        }
+
+    }
+}
